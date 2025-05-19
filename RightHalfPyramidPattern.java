9db4e01d67539904b9386eec907fb86dbe57a276
amrit <amritsuman1996@gmail.com>
2025-05-19 23:25:08 +0530

    RightHalfPyramidPattern_v2

--- RightHalfPyramidPattern.java
+++ RightHalfPyramidPattern.java
@@ -1,6 +1,10 @@
 // Program to Print Right Half Pyramid Pattern (Star Pattern)
 // Given an integer N, print N rows of right half pyramid pattern. In right half pattern of N rows, the first row has 1 star, second row has 2 stars and so on till the Nth row which has N stars. All the stars are left aligned.
-
+// *
+// **
+// ***
+// ****
+// *****
 public class RightHalfPyramidPattern {
     public static void main(String[] args) {
         for(int i = 0; i < 5; i++){
