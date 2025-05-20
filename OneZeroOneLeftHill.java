6d7be0d9fab1ca89de15c588a76b0142e32a5b78
amrit <amritsuman1996@gmail.com>
2025-05-20 22:11:49 +0530

    OneZeroOneLeftHill_v1

---
+++ OneZeroOneLeftHill.java
@@ -0,0 +1,26 @@
+// 1
+// 01
+// 101
+// 0101
+// 10101
+
+public class OneZeroOneLeftHill {
+
+    public static void main(String[] args) {
+        
+        int n = 5;
+        int start;
+        for(int i = 1; i <= n; i++){
+            if(i % 2 == 0){
+                start = 0;
+            }else{
+                start = 1;
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print(start);
+                start = 1 - start;
+            }
+            System.out.println();
+        }
+    }
+}
