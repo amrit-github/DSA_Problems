becb79d5949eeafc52f74e75d6c315aa83f69462
amrit <amritsuman1996@gmail.com>
2025-05-25 08:05:32 +0530

    Striver_v2

--- StriverSheet.java
+++ StriverSheet.java
@@ -104,10 +104,36 @@
 // C C C 
 // D D D D 
 // E E E E E 
+
+// 17
+//      A     
+//     ABA    
+//    ABCBA   
+//   ABCDCBA  
+//  ABCDEDCBA 
+
+// 18
+// E
+// DE
+// CDE
+// BCDE
+// ABCDE
+
+// 19
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
 public class StriverSheet {
     public static void main(String[] args) {
         
-        print16(5);
+        print19(5);
     }
 
     static void print3(int n){
@@ -268,11 +294,74 @@ public class StriverSheet {
             System.out.println();
         }
     }
+    static void print17(int n){
+        for(int i = 0; i < n; i++){
+            
+            for(int j = 1; j < n-i+1; j++){
+                System.out.print(" ");
+            }
 
+            char ch = 'A';
+            int breakpoint = (2*i + 1)/2;
+            for(int j = 1; j <= 2*i + 1; j++){
+                System.out.print(ch);
+                if(j <= breakpoint){
+                    ch++;
+                }else{
+                    ch--;
+                }
+            }
+
+            for(int j = 1; j <= n-i; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    static void print18(int n){
+        for(int i = 0; i < n; i++){
+            char start = (char)('E' - i);
+            for(int j = 0; j <= i; j++){
+                System.out.print(start);
+                start++;
+            }
+            System.out.println();
+        }
+    }
+    static void print19(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j + i < n; j++){
+                System.out.print("*");
+            }
+            for(int j = 0; j < 2*i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j + i < n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        int intS = 2*n - 2;
+        for(int i = 5; i < 2*n; i++){
+            for(int j = 1; j <= i-n+1; j++){
+                System.out.print("*");
+            }
+
+            for(int j = 1; j <= intS; j++){
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= i-n+1; j++){
+                System.out.print("*");
+            }
+            intS -= 2;
+
+            System.out.println();
+        }
+    }
  
-
-
-
+  
 
 
 
@@ -281,3 +370,4 @@ public class StriverSheet {
 
 
 }
+ 
