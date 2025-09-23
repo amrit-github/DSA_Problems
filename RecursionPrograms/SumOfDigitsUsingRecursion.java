2b8d1b088ed6bbea9c6478b34f5674561bccbd71
amritsuman <amritsuman1996@gmail.com>
2025-09-23 10:00:17 +0530

    DSA_23_09

---
+++ RecursionPrograms/SumOfDigitsUsingRecursion.java
@@ -0,0 +1,24 @@
+package RecursionPrograms;
+
+public class SumOfDigitsUsingRecursion {
+
+    public static void main(String[] args) {
+        
+        int sum = 0;
+        //System.out.println(sumOfDigits(sum, 9987));
+        System.out.println(sumOfD(9987));
+    }
+
+    static int sumOfDigits(int sum, int n){
+        if(n == 0) return sum;
+        sum += n % 10;
+        n /= 10;
+        return sumOfDigits(sum, n);
+    }
+
+    //2nd approach
+    static int sumOfD(int n){
+        if(n == 0) return 0;
+        return sumOfD(n/10) + n%10;
+    }
+}
